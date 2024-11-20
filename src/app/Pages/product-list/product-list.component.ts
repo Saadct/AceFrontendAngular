90b23c5fff730c83b8471ddc1c7491d9e6792da0
Saadct <92342579+Saadct@users.noreply.github.com>
2024-11-20 16:39:46 +0100

    Add notification after deleting a product

--- src/app/Pages/product-list/product-list.component.ts
+++ src/app/Pages/product-list/product-list.component.ts
@@ -78,6 +78,7 @@ export class ProductListComponent implements OnInit {
       this.productService.deleteProduct(id).subscribe(
         () => {
           this.loadProducts();
+          this.showNotification('Produit supprimé avec succées !', 'success')
         },
         (error) => {
           this.showNotification('Erreur lors de la suppression du produit !', 'error');
